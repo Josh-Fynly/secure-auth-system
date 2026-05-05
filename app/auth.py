433353a33d22f458c1988e7d041b4cc4b7acb09a
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-05-05 17:07:30 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,16 @@
+from sqlalchemy.orm import Session
+from . import models, security
 
+def register_user(db: Session, username: str, password: str):
+    hashed = security.hash_password(password)
+
+    user = models.User(
+        username=username,
+        password=hashed
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
