b0988ffca1e704af841edc46af17fb163a240b1e
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-05-05 16:54:30 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    failed_attempts = Column(Integer, default=0)
+    is_locked = Column(Boolean, default=False)
