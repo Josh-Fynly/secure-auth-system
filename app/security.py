110aa5f79c1edd655e589000cc4aadb0f7daa6e1
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-05-20 20:55:56 +0100

    Refactor password functions and add JWT token creation
    Refactor password hashing and verification functions for readability and add JWT access token creation.

--- app/security.py
+++ app/security.py
@@ -1,7 +1,31 @@
 import bcrypt
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "supersecretkey"
+ALGORITHM = "HS256"
 
 def hash_password(password: str):
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    return bcrypt.hashpw(
+        password.encode(),
+        bcrypt.gensalt()
+    ).decode()
 
 def verify_password(password: str, hashed: str):
-    return bcrypt.checkpw(password.encode(), hashed.encode())
+    return bcrypt.checkpw(
+        password.encode(),
+        hashed.encode()
+    )
+
+def create_access_token(data: dict):
+    payload = data.copy()
+
+    payload["exp"] = (
+        datetime.utcnow() + timedelta(minutes=15)
+    )
+
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
